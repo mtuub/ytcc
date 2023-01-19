7912b90034275f7e4a7dcc986cac93ee47e6b132
mtuu <upmoris@gmail.com>
2023-01-19 12:42:44 +0400

    19 jan

--- email.js
+++ email.js
@@ -1,9 +1,9 @@
 const fs = require("fs/promises");
 const { verifyEmail } = require("./utils");
 
-const limit = 12;
-const start_count = 100000;
-const end_count = 300000;
+const limit = 55;
+const start_count = 75000;
+const end_count = 100000;
 
 (async () => {
   const channel_email_datas = JSON.parse(
